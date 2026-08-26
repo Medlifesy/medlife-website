a79daf6f171e847f6c499c01e2733b78903dd2f3
Medlifesy <info@medlifesy.org>
2026-08-26 23:44:29 +0300

    fix(articles): remove conflicting tension headache function

--- functions/articles/tension-headache.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest({ request, env }) {
-  const url = new URL(request.url);
-  const target = new URL('/articles/tension-headache.html', url.origin);
-  return env.ASSETS.fetch(target);
-}
