72de39796c316a337d2a2fc1f79b1a74bd5fc7bc
Medlifesy <info@medlifesy.org>
2026-08-26 23:43:20 +0300

    fix(articles): serve tension headache through stable route

---
+++ functions/articles/tension-headache.js
@@ -0,0 +1,5 @@
+export async function onRequest({ request, env }) {
+  const url = new URL(request.url);
+  const target = new URL('/articles/tension-headache.html', url.origin);
+  return env.ASSETS.fetch(target);
+}
